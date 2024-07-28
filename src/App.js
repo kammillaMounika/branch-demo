8275b0486e450409f0576f6f2f1a1af0354c1ec7
kammillaMounika <kammillamounika117@gmail.com>
2024-07-28 13:19:03 +0530

    Structure of app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Attendence from "./Attendence";
+import Classes from "./Classes";
+
+
+function App() {
+  return (
+    <>
+    <h1>Kodnest App</h1>
+    <Classes />
+    <Attendence />
+    
+    </>
+  );
+}
+
+export default App;
